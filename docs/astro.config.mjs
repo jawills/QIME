e3052a41b8a88d6bdf00c019de5649f3900822da
jawills <justin@1sync.co>
2024-01-13 14:53:22 -0300

    added github documentation

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://jawills.github.io',
+	base: '/QIME',
 	integrations: [
 		starlight({
 			title: 'QIME',
