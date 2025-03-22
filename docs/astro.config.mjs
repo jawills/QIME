107352fae197ae6b16c95386d7a439928a587918
jawills <justin@1sync.co>
2025-03-22 17:00:18 +0000

    Update domain name

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,28 +1,27 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://qime.1sync.co',
-	integrations: [
-		starlight({
-			title: 'QIME',
-			social: {
-				github: 'https://github.com/jawills/qime',
-				youtube: 'https://www.youtube.com/channel/UCZvztpm28HOreQqX5nmOlYQ',
-				linkedin: 'https://www.linkedin.com/in/justinwillsxyz/',
-			},
-			sidebar: [
-				{
-					label: 'Start Here',
-					autogenerate: { directory: 'start-here' },
-
-				},
-				{
-					label: 'Objects',
-					autogenerate: { directory: 'objects' },
-				},
-			],
-		}),
-	],
+  site: "https://qime.app",
+  integrations: [
+    starlight({
+      title: "QIME",
+      social: {
+        github: "https://github.com/jawills/qime",
+        youtube: "https://www.youtube.com/channel/UCZvztpm28HOreQqX5nmOlYQ",
+        linkedin: "https://www.linkedin.com/in/justinwillsxyz/"
+      },
+      sidebar: [
+        {
+          label: "Start Here",
+          autogenerate: { directory: "start-here" }
+        },
+        {
+          label: "Objects",
+          autogenerate: { directory: "objects" }
+        }
+      ]
+    })
+  ]
 });
